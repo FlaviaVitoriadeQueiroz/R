9634240c341d7ff67745de2373ac30d54b484f95
Flávia Vitória de Queiroz <flaviavitoriaqueiroz@gmail.com>
2026-04-13 12:53:58 -0300

    Add files via upload

---
+++ Cursos/introductiontor.r
@@ -0,0 +1,74 @@
+# An addition
+5 + 5 
+
+# A subtraction
+5 - 5 
+
+# A multiplication
+3 * 5
+
+ # A division
+(5 + 5) / 2 
+
+# Exponentiation
+2 ^ 5
+
+# Modulus
+28 %% 6
+
+
+
+# Atribuietion a value to a variable
+x <- 5
+y <- 10
+
+x + y
+
+
+
+# Assign the value 5 to the variable my_apples
+my_apples <- 5
+
+# Print out the value of the variable my_apples
+my_apples #in python we would print and in r we just write the name of the variable to see its value
+
+
+
+# Assign a value to the variables my_apples and my_oranges
+my_apples <- 5
+my_oranges <- 6
+
+# Add these two variables together
+my_fruit <- my_apples + my_oranges
+
+# Create the variable my_fruit
+my_fruit
+
+
+
+# Change my_numeric to be 42
+my_numeric <- 42 #int
+
+# Change my_character to be "universe"
+my_character <- "universe" #string
+
+# Change my_logical to be FALSE
+my_logical <- FALSE #bool
+
+
+
+# Declare variables of different types
+my_numeric <- 42
+my_character <- "universe"
+my_logical <- FALSE 
+
+# Check class of my_numeric
+class(my_numeric) #in python be would use type() function, but in R we use class() function
+
+# Check class of my_character
+class(my_character)
+
+# Check class of my_logical
+class(my_logical)
+
+
